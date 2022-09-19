b1621a1cad5b9d7d09c56a0693500c3e3f917949
max <max@karl.wf>
2022-09-19 20:07:11 +0200

    Switch from print to logging. Rework (re-)connect code

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
+#[macro_use]
+extern crate log;
+
 use dxcllistener::Listener;
 use lazy_static::lazy_static;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
+use simplelog::{
+    format_description, ColorChoice, CombinedLogger, ConfigBuilder, LevelFilter, TermLogger,
+    TerminalMode,
+};
 use std::fs::File;
 use std::io;
 use std::io::BufReader;
 use std::path::Path;
 use std::process;
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::{mpsc, Arc};
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct Configuration {
     constrings: Vec<String>,
+    reconnect: bool,
+    retries: u64,
+    backoff: u64,
 }
 
 /// Parse configuration
@@ -44,36 +54,66 @@ fn parse_constring(raw: &str) -> Option<Listener> {
     None
 }
 
+/// Initialize logging
+fn init_logging() {
+    let log_config = ConfigBuilder::new()
+        .set_time_format_custom(format_description!(
+            "[day].[month].[year] [hour]:[minute]:[second]"
+        ))
+        .build();
+
+    CombinedLogger::init(vec![
+        TermLogger::new(
+            LevelFilter::Info,
+            log_config,
+            TerminalMode::Mixed,
+            ColorChoice::Auto,
+        ),
+        /*WriteLogger::new(
+            LevelFilter::Info,
+            log_config,
+            File::create(Path::new(&config.log_dir).join(&config.log_file))
+                .expect("Failed to create log file"),
+        ),*/
+    ])
+    .unwrap();
+}
+
 fn main() {
+    init_logging();
+
     // Read json configuration
     let config =
         parse_config(Path::new("dxclrecorder.json")).expect("Failed to read configuration");
 
     // Parse connection strings
-    let mut listeners: Vec<Listener> = Vec::new();
+    let listeners: Arc<Mutex<Vec<Listener>>> = Arc::new(Mutex::new(Vec::new()));
     for constring in config.constrings.iter() {
         match parse_constring(constring) {
-            Some(info) => listeners.push(info),
+            Some(info) => {
+                listeners.lock().unwrap().push(info);
+            }
             None => {
-                eprintln!("Found invalid connection string: '{}'", constring);
+                error!("Found invalid connection string: {}", constring);
                 process::exit(1);
             }
         }
     }
 
     // Communication channel between listeners and receiver
-    let (tx, rx) = mpsc::channel();
-
-    // Start listeners
-    for lis in listeners.iter_mut() {
-        lis.listen(tx.clone()).unwrap();
-    }
+    let (tx, rx) = mpsc::channel::<dxcllistener::Spot>();
 
     // Handle incoming spots
     thread::spawn(move || {
+        debug!("Receiving thread started");
+
+        //let mut file = File::create("foo.txt").unwrap();
         while let Ok(spot) = rx.recv() {
+            //file.write_all(spot.to_json().as_bytes()).unwrap();
             println!("{}", spot.to_json());
         }
+
+        debug!("Receiving thread finished");
     });
 
     // Stop signal
@@ -82,32 +122,53 @@ fn main() {
     // Register ctrl-c handler to stop listeners
     let sig = signal.clone();
     ctrlc::set_handler(move || {
-        println!("Ctrl-C caught");
+        debug!("Ctrl-C caught");
         sig.store(false, Ordering::Relaxed);
     })
     .expect("Failed to listen on Ctrl-C");
 
+    // Start all listeners and remove from list afterwards
+    listeners.lock().unwrap().retain_mut(|l| {
+        connect_listener(
+            Listener::new(l.host.clone(), l.port, l.callsign.clone()),
+            listeners.clone(),
+            config.clone(),
+            tx.clone(),
+        );
+        false
+    });
+
     // Main process loop
-    while !listeners.is_empty() {
+    loop {
         // Check for application stop request
         if !signal.load(Ordering::Relaxed) {
-            for l in listeners.iter_mut() {
+            let mut lis = listeners.lock().unwrap();
+
+            debug!("Request stop of listeners");
+            for l in lis.iter_mut() {
                 l.request_stop();
             }
-            for l in listeners.iter_mut() {
+            debug!("Join listeners");
+            for l in lis.iter_mut() {
                 l.join().unwrap();
             }
             break;
         }
 
         // Check for unexpectedly stopped listeners and remove them from the list of active listeners
-        listeners.retain_mut(|l| {
+        listeners.lock().unwrap().retain_mut(|l| {
             if !l.is_running() {
                 let res = l.join().unwrap_err();
-                println!(
+                warn!(
                     "Listener {}@{}:{} stopped unexpectedly ({})",
                     l.callsign, l.host, l.port, res
                 );
+
+                if config.reconnect {
+                    let dead = Listener::new(l.host.clone(), l.port, l.callsign.clone());
+                    connect_listener(dead, listeners.clone(), config.clone(), tx.clone());
+                }
+
                 return false;
             }
             true
@@ -117,4 +178,31 @@ fn main() {
     }
 
     process::exit(0);
+}
+
+fn connect_listener(
+    mut listener: Listener,
+    listeners: Arc<Mutex<Vec<Listener>>>,
+    config: Configuration,
+    tx: mpsc::Sender<dxcllistener::Spot>,
+) {
+    thread::spawn(move || {
+        let mut recon_ctr = config.retries;
+        loop {
+            info!("Try to connect listener {}", listener);
+            if listener.listen(tx.clone()).is_ok() {
+                info!("Listener {} connected", listener);
+                listeners.lock().unwrap().push(listener);
+                break;
+            }
+
+            recon_ctr -= 1;
+            if recon_ctr == 0 {
+                error!("Failed to connect listener {}", listener);
+                break;
+            }
+
+            std::thread::sleep(std::time::Duration::from_secs(config.backoff));
+        }
+    });
 }
